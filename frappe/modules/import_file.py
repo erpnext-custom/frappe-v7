205ec0c59e876dc1193d8aa746f2df3b8abcec7f
Kinley Tshering <kxt5258@gmail.com>
2019-01-22 19:51:45 +0600

    permissions reset while migrate

--- frappe/modules/import_file.py
+++ frappe/modules/import_file.py
@@ -48,6 +48,9 @@ def import_file_by_path(path, force=False, data_import=False, pre_process=None):
 				db_modified = frappe.db.get_value(doc['doctype'], doc['name'], 'modified')
 				if db_modified and doc.get('modified')==get_datetime_str(db_modified):
 					return False
+				else:
+					if doc['doctype'] == "DocType":
+						frappe.permissions.reset_perms(doc['name'])
 
 			original_modified = doc.get("modified")
 
